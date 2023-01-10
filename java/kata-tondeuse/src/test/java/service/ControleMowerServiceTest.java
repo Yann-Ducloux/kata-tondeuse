4355d5dce2aac3509eba7820d333a1559c19de3f
“Yann-Ducloux” <duclou.y@gmail.com>
2023-01-10 11:57:35 +0100

    fix : correction du test

--- java/kata-tondeuse/src/test/java/service/ControleMowerServiceTest.java
+++ java/kata-tondeuse/src/test/java/service/ControleMowerServiceTest.java
@@ -29,39 +29,6 @@ class ControleMowerServiceTest {
         String pointFinalForFirstTondeuseExpected = "1 3 N";
         String pointFinalForSecondeTondeuseExpected = "5 1 E";
         int numberOfTondeuseExpected = 2;
-        FileData fileDataExpected = new FileData();
-        CoordinateMax coordinateMax = new CoordinateMax();
-        coordinateMax.setxMax(5);
-        coordinateMax.setyMax(5);
-        List<Mower> mowers = new ArrayList<Mower>();
-
-        Mower mowerFirst = new Mower();
-        Position positionFirst = new Position();
-        positionFirst.setX(1);
-        positionFirst.setY(2);
-        Instruction[] instructionsFirst = {Instruction.G, Instruction.A, Instruction.G,
-                Instruction.A, Instruction.G, Instruction.A,
-                Instruction.G, Instruction.A, Instruction.A};
-        mowerFirst.setPosition(positionFirst);
-        mowerFirst.setDirection(Direction.N);
-        mowerFirst.setInstructions(instructionsFirst);
-        mowers.add(mowerFirst);
-
-        Mower mowerSecond = new Mower();
-        Position positionSecond = new Position();
-        positionSecond.setX(3);
-        positionSecond.setY(3);
-        Instruction[] instructionsSecond = {Instruction.A, Instruction.A, Instruction.D,
-                Instruction.A, Instruction.A, Instruction.D, Instruction.A,
-                Instruction.D, Instruction.D, Instruction.A};
-        mowerSecond.setPosition(positionSecond);
-        mowerSecond.setDirection(Direction.E);
-        mowerSecond.setInstructions(instructionsSecond);
-        mowers.add(mowerSecond);
-
-        fileDataExpected.setCoordinateMax(coordinateMax);
-        fileDataExpected.setMowers(mowers);
-
         String coordinate = "5 5";
         String directionFirst = "1 2 N";
         String instructionsLineFirst = "GAGAGAGAA";
