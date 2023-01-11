1b507d58109aa8cea3ce992a6a3b9d9835459644
“Yann-Ducloux” <duclou.y@gmail.com>
2023-01-11 11:16:04 +0100

    fix: mini correction

--- kata-tondeuse/src/test/java/service/ControleMowerServiceTest.java
+++ kata-tondeuse/src/test/java/service/ControleMowerServiceTest.java
@@ -17,7 +17,7 @@ class ControleMowerServiceTest {
     /**
      * le saut de ligne.
      */
-    private static final String LINE_BREAK = "\n";
+    private static final String LINE_BREAK = "\r\n";
     /**
      * the controle Mower Service.
      */
